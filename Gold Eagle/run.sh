6513ef0bca5bfc77b418b47a6b8e7404681a0f82
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-25 14:34:21 +0700

    Update run.sh

--- Gold Eagle/run.sh
+++ Gold Eagle/run.sh
@@ -30,16 +30,14 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "timeZone": "en-US",
+  "rotateProxy": false,
+  "skipInvalidProxy": false,
+  "proxyRotationInterval": 2,
+  "delayEachAccount": [1, 1],
+  "timeToRestartAllAccounts": 300,
+  "howManyAccountsRunInOneTime": 10,
+  "doTasks": true
 }
 EOL
 }
